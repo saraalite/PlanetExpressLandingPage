2f965a04f756edbc8308417f81973829d343f452
Sara <sara.alite@maticapartners.com>
2021-09-04 17:31:29 +0200

    Test get public image from PUBLIC_URL in github pages

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
       <header>
         <h1 className="Title" >PLANET EXPRESS</h1>
         <h2>Envío intergaláctico de todo tipo de mercancía</h2>
-        <img className="logo_pe" src="/logo_pe.png" alt="Planet express logo. Big spaceship."/>
+        <img className="logo_pe" src={process.env.PUBLIC_URL + '/logo_pe.png'} alt="Planet express logo. Big spaceship."/>
       </header>
     </div>
   );
